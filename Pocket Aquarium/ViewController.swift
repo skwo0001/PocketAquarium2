c4555523c87258ec2c904a9a3a25c33227781804
Liwen <llia0006@student.monash.edu>
2018-10-03 17:53:49 +1000

    Initial Commit

---
+++ Pocket Aquarium/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Pocket Aquarium
+//
+//  Created by Liangliwen on 3/10/18.
+//  Copyright © 2018 Monash University. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
